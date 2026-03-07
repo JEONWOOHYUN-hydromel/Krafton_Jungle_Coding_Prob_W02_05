ee30e783a4061a09f24905e3ce7d6b005aeb2242
woohyun <haeli0312@gmail.com>
2026-03-07 23:56:36 +0900

    2주차 4번 문제 완료

--- week2/basic/04_brute_force.py
+++ week2/basic/04_brute_force.py
@@ -44,7 +44,10 @@ def find_two_sum_pairs(nums, target):
     ## 외부 반복문: i는 0부터 n-1까지
     ## 내부 반복문: j는 i+1부터 n까지 (중복 방지)
     ## nums[i] + nums[j]가 target과 같으면 (i, j)를 결과에 추가
-    pass  
+    for i in range(len(nums)):
+        for j in range(len(nums)-i-1):
+            if nums[i] + nums[i+j+1] == target:
+                pairs.append((i, i+j+1))
     
     return pairs
 
