64b385a2fdc46c1d2ec06dde8cc79527f1375f2f
woohyun <haeli0312@gmail.com>
2026-03-09 22:52:51 +0900

    feat: 백준 4344번 완료

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,20 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+case_num = int(input())
+for i in range(n):
+    arr = list(map(int, input().split()))
+    
+    score_num = arr.pop(0)
+    total = 0
+    #합계
+    for score in arr:
+        total += score
+    #평균
+    score_avg = total / len(arr)
+    #평균이상 구하기
+    over_avg = 0
+    for score in arr:
+        if score > score_avg:
+            over_avg += 1
+    
+    print(f"{over_avg/len(arr)*100:.3f}%")
