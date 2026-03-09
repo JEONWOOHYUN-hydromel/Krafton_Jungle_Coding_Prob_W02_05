691816c21320c0763512be3a26a9609614a09195
woohyun <haeli0312@gmail.com>
2026-03-09 21:15:49 +0900

    feat: 백준 2562번 완료

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,10 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+arr = [int(input()) for _ in range(9)]
+max_num = arr[0]
+max_iter = 1
+for i in range(1,9):
+    if arr[i] > max_num:
+        max_num = arr[i]
+        max_iter = i+1
+print(f"{max_num}\n{max_iter}")
