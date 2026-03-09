a5fabe376c2b9320a3566668bbd7daf8ca036de2
woohyun <haeli0312@gmail.com>
2026-03-09 23:08:41 +0900

    feat: 백준 2675번 완료

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,10 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+case_num = int(input())
+for i in range(case_num):
+    arr = input().split()
+    rep_num = int(arr[0])
+    input_str = arr[1]
+    for char in input_str:
+        print(char*rep_num, end="")
+    print("")
