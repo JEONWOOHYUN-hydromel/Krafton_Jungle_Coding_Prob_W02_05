9b0e296a923cdc382a63ec41062f5cf8b3c16b6c
woohyun <haeli0312@gmail.com>
2026-03-10 23:34:27 +0900

    feat: 백준 1016번 완료

--- week2/problem-solving/난이도상_정수론_제곱ㄴㄴ수_골드1.py
+++ week2/problem-solving/난이도상_정수론_제곱ㄴㄴ수_골드1.py
@@ -1,2 +1,15 @@
 # 정수론 - 제곱 ㄴㄴ 수 (백준 골드1)
 # 문제 링크: https://www.acmicpc.net/problem/1016
+min_num, max_num = map(int, input().split())
+check = [False] * (max_num - min_num + 1)
+
+i = 2
+while i**2 <= max_num:
+    sq = i**2
+    for j in range((min_num//sq)-1, (max_num//sq)+1):
+        num = j * sq
+        if min_num <= num <= max_num:
+            check[num - min_num] = True
+    i += 1
+
+print(check.count(False))
