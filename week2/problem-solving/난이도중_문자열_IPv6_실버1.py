27acce3a6f079e08fd85e7bbdb52b2c27d1b6e76
woohyun <haeli0312@gmail.com>
2026-03-10 22:32:15 +0900

    feat: 백준 3107번 완료

--- week2/problem-solving/난이도중_문자열_IPv6_실버1.py
+++ week2/problem-solving/난이도중_문자열_IPv6_실버1.py
@@ -1,2 +1,33 @@
 # 문자열 - IPv6 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/3107
+
+addr = input().strip()
+
+idx_r2 = addr.find("::")
+prev_num = 0
+
+if idx_r2 == -1:
+    pass
+elif idx_r2 == 0:
+    addr = addr.replace("::", "0000:")
+else:
+    addr = addr.replace("::", ":0000:")
+    for i in range(idx_r2):
+        if addr[i] == ":":
+            prev_num += 1
+
+unzip_sp = []
+zip_sp = addr.split(":")
+
+for word in zip_sp:
+    if word == "":
+        continue
+    unzip_sp.append("0" * (4 - len(word)) + word)
+
+while len(unzip_sp) < 8:
+    unzip_sp.insert(prev_num + 1, "0000")
+
+unzip = ":".join(unzip_sp)
+print(unzip)
+
+
