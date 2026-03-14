2d1789cdc1e33d00286ac587b25c01b1b8474422
woohyun <haeli0312@gmail.com>
2026-03-14 20:14:47 +0900

    백준 2295번 완료

--- week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
+++ week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
@@ -1,2 +1,17 @@
 # 해시 테이블 - 세 수의 합 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/2295
+n = int(input())
+arr = sorted(int(input()) for _ in range(n))
+
+set_sum = set()
+
+for i in range(n):
+    for j in range(i, n):
+        set_sum.add(arr[i] + arr[j])
+
+
+for k in arr[::-1]:
+    for z in arr[::-1]:
+        if (k-z) in set_sum:
+            print(k)
+            exit()
