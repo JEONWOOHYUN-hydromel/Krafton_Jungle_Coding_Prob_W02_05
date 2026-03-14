505268cbd7c9c34ef3b955231ee146a9f5b07112
woohyun <haeli0312@gmail.com>
2026-03-14 14:53:13 +0900

    백준 1920번 완료

--- week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
+++ week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
@@ -1,2 +1,31 @@
 # 이분탐색 - 수 찾기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/1920
+n = int(input())
+n_arr = list(map(int, input().split()))
+m = int(input())
+m_arr = list(map(int, input().split()))
+
+n_arr.sort()
+
+def binary_search(start, end, target):
+    if start > end:
+        return False
+    elif start == end:
+        if target == n_arr[start]:
+            return True
+        else: 
+            return False
+    else:
+        mid = start + (end - start) // 2
+        if n_arr[mid] == target:
+            return True
+        elif n_arr[mid] > target:
+            return binary_search(start, mid-1, target)
+        else:
+            return binary_search(mid+1, end, target)
+        
+for num in m_arr:
+    if binary_search(0, n-1, num):
+        print(1)
+    else:
+        print(0)
