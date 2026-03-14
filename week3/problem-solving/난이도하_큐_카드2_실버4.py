8dc94803986b49c904eb4c84690d7af79aad1c76
woohyun <haeli0312@gmail.com>
2026-03-14 14:35:07 +0900

    백준 2164번 완료

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,18 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+from collections import deque
+
+n = int(input())
+arr = [i for i in range(1, n+1)]
+que = deque(arr)
+
+num = -1
+isOdd = True
+while que:
+    num = que.popleft()
+    if isOdd:
+        isOdd = False
+    else:
+        que.append(num)
+        isOdd = True
+print(num)
