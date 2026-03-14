a51beb9852e6de552a471b7e51b516d2a45a64ad
woohyun <haeli0312@gmail.com>
2026-03-14 13:34:37 +0900

    백준 9933번 완료

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,19 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+n = int(input())
+arr = [input().strip() for _ in range(n)]
+
+# arr = ["had", "adsfa", "poiop"]
+
+dic = {}
+for word in arr:
+
+    if word == word[::-1]:
+        print(len(word), word[len(word)//2])
+        break
+
+    if word not in dic:
+        dic[word[::-1]] = word[len(word)//2]
+    else:
+        print(len(word), dic[word])
+        break
