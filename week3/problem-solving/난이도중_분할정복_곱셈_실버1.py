87a64a5b1188f9c57e0003b3773aaeaff8b7ebbb
woohyun <haeli0312@gmail.com>
2026-03-16 15:42:46 +0900

    백준 1629번 완료

--- week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
+++ week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
@@ -1,2 +1,19 @@
 # 분할정복 - 곱셈 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1629
+num_input = list(map(int, input().split()))
+A = num_input[0]
+B = num_input[1]
+C = num_input[2]
+
+def power(a,b):
+    if b == 1 :
+        return a % C
+    
+    h = power(a, b//2)
+
+    if b % 2:
+        return (h*h*a) % C
+    else: 
+        return (h*h) % C
+    
+print(power(A,B))
