a5f562bb680401e836333d205f3fe72b13d96048
woohyun <haeli0312@gmail.com>
2026-03-16 14:56:12 +0900

    백준 2493번 완료

--- week3/problem-solving/난이도중_스택_탑_골드5.py
+++ week3/problem-solving/난이도중_스택_탑_골드5.py
@@ -1,2 +1,19 @@
 # 스택 - 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2493
+n = int(input())
+towers = list(map(int, input().split()))
+
+stack = []
+result = []
+for i in range(n):
+    while stack and towers[stack[-1]] <= towers[i]:
+        stack.pop()
+
+    if stack:
+        result.append(stack[-1] + 1)
+    else:
+        result.append(0)
+
+    stack.append(i)
+
+print(" ".join(map(str, result)))
