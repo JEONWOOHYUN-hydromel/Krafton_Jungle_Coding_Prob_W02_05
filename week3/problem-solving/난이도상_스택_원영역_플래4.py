ff7ad077af05ae2bef5d065e6e99fd9d708a5f34
woohyun <haeli0312@gmail.com>
2026-03-17 15:57:38 +0900

    feat: 백준 10000번 완료

--- week3/problem-solving/난이도상_스택_원영역_플래4.py
+++ week3/problem-solving/난이도상_스택_원영역_플래4.py
@@ -1,2 +1,40 @@
 # 스택 - 원 영역 (백준 플래4)
 # 문제 링크: https://www.acmicpc.net/problem/10000
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+events = []
+for _ in range(n):
+    x, r = map(int, input().split())
+    l = x - r
+    rr = x + r
+
+    events.append((l, 1, -rr))  # start
+    events.append((rr, 0, -l))  # end
+
+events.sort()
+
+result = 1
+stack = []
+for event in events:
+    loc = event[0]
+    #start
+    if event[1] == 1:
+        stack.append([loc, loc]) #[start,last]
+    
+    #end
+    elif event[1] == 0:
+        result+=1
+        cur = stack.pop()
+
+        if cur[1] == loc:
+            result += 1
+
+        if stack:
+            if stack[-1][1] == cur[0]:
+                stack[-1][1] = loc
+            
+
+print(result)
