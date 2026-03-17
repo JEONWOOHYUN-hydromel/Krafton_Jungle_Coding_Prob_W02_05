eadc34cd62fe20ec99b35838e8b5e8ed1309bdee
woohyun <haeli0312@gmail.com>
2026-03-17 19:25:50 +0900

    feat: Leetcode : Longest Consecutive Sequence 완료

--- week3/problem-solving/Extra_해시테이블_LongestConsecutiveSequence.py
+++ week3/problem-solving/Extra_해시테이블_LongestConsecutiveSequence.py
@@ -1,2 +1,23 @@
 # 해시 테이블 - Longest Consecutive Sequence
 # 문제 링크: https://leetcode.com/problems/longest-consecutive-sequence/description/?envType=study-plan-v2&envId=top-interview-150
+
+num = [0,3,7,2,5,8,4,6,0,1]
+num.sort()
+max = 0
+connect = 1
+cur = num[0]
+for n in num:
+    if n == cur:
+        pass
+    elif n == cur + 1:
+        connect += 1
+    else:
+        if connect > max:
+            max = connect
+        connect = 1
+    cur = n
+
+if connect > max:
+    max = connect
+    
+print(max)
