74d96100add999663ad28dc77a417fa483e5636e
woohyun <haeli0312@gmail.com>
2026-03-19 22:21:47 +0900

    feat: 백준 14244번 완료

--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,14 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+total, leaf_num = map(int, input().split())
+turn = total-leaf_num+1
+
+for i in range(total):
+    if i < turn:
+        print(i, i+1)
+    elif i > turn:
+        dif = i - turn
+        if i-dif < 2:
+            print(0, i)
+        else:
+            print(i-dif-1 , i)
