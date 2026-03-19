90806956feea50a9a7ce46105c78364b27f1d94b
woohyun <haeli0312@gmail.com>
2026-03-19 21:19:15 +0900

    feat: week4 6번 완료

--- week4/basic/06_topological_sort.py
+++ week4/basic/06_topological_sort.py
@@ -40,20 +40,36 @@ def topological_sort(vertices, edges):
         위상 정렬 순서
     """
     # TODO: 그래프와 진입 차수 초기화
-    pass
+    graph = {}
+    for v in range(vertices):
+        graph[v] = [0, []]
     
     # TODO: 그래프 구성 및 진입 차수 계산
-    pass
-    
+    for arw in edges:
+        key_num = arw[0]
+        value_num = arw[1]
+
+        graph[value_num][0] += 1
+        graph[key_num][1].append(value_num)
+
     # TODO: 진입 차수가 0인 정점들을 큐에 추가
-    pass
+    que = deque()
+    for i in range(vertices):
+        if graph[i][0] == 0:
+            que.append(i)
     
     result = []
     
     # TODO: 큐가 빌 때까지 반복
     ## 큐에서 정점 꺼내기
     ## 인접한 정점들의 진입 차수 감소
-    pass
+    while que:
+        cur_nod = que.popleft()
+        result.append(cur_nod)
+        for nei_nod in graph[cur_nod][1]:
+            graph[nei_nod][0] -= 1
+            if graph[nei_nod][0] == 0:
+                que.append(nei_nod)
     
     return result
 
