3455d01a16867d80352508920d10a0d7e67055ab
woohyun <haeli0312@gmail.com>
2026-03-20 16:37:58 +0900

    faet: 백준 11725번 완료

--- week4/problem-solving/난이도중_트리_트리의부모찾기_실버2.py
+++ week4/problem-solving/난이도중_트리_트리의부모찾기_실버2.py
@@ -1,2 +1,28 @@
 # 트리 - 트리의 부모 찾기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/11725
+import sys
+input = sys.stdin.readline
+
+sys.setrecursionlimit(10**6)
+
+num_nod = int(input())
+edges = {}
+for i in range(1, num_nod+1):
+    edges[i] = []
+
+for _ in range(num_nod-1):
+    nod1, nod2 = map(int, input().split())
+    edges[nod1].append(nod2)
+    edges[nod2].append(nod1)
+
+par = {1:0}
+
+def dfs(nod):    
+    for nxt in edges[nod]:
+        if nxt not in par:
+                par[nxt] = nod
+                dfs(nxt)
+
+dfs(1)
+for j in range(2, num_nod+1):
+    print(par[j])
--- week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
@@ -1,2 +1,31 @@
 # 그래프, DFS, BFS - 바이러스 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/2606
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+num_com = int(input())
+num_route = int(input())
+
+route = {}
+for i in range(1, num_com+1):
+    route[i] = []
+
+for _ in range(num_route):
+    com1, com2 = map(int, input().split())
+    route[com1].append(com2)
+    route[com2].append(com1)
+
+que = deque()
+que.append(1)
+virused = []
+while que:
+    new_com = que.popleft()
+    virused.append(new_com)
+
+    for c in route[new_com]:
+        if c not in que and c not in virused:
+            que.append(c)
+
+print(len(virused)-1)
