39f88db3ed7153e68ec261eff7bea5fd7128776c
woohyun <haeli0312@gmail.com>
2026-03-20 11:40:00 +0900

    feat: 백준 16173번 완료

--- week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
@@ -1,2 +1,32 @@
 # 그래프, DFS, BFS - 점프왕 쩰리 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/16173
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+
+que = deque()
+que.append([0, 0])
+while que:
+    cur = que.popleft()
+    val = board[cur[0]][cur[1]]
+    if val == 0:
+        pass
+    elif val == -1:
+        print("HaruHaru")
+        sys.exit()
+
+    else:
+        next0 = cur[0]+val
+        next1 = cur[1]+val
+
+        if next0 < n:
+            que.append([next0, cur[1]])
+        
+        if next1 < n:
+            que.append([cur[0], next1])
+    
+print("Hing")
