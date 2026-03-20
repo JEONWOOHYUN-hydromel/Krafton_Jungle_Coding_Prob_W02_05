5207433cf572c1dfc86e51747f8e5c9ddf7a8f00
woohyun <haeli0312@gmail.com>
2026-03-20 21:36:47 +0900

    feat: 백준 11725번 완료

--- week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
+++ week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
@@ -1,2 +1,29 @@
 # 트리 - 이진 검색 트리 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/5639
+import sys
+sys.setrecursionlimit(10**6)
+pre = list(map(int, sys.stdin.read().split()))
+
+def post(start, end):
+    #base case
+    if start >= end:
+        return
+    
+    #where to cut
+    root = pre[start]
+    mid = end
+    for i in range(start+1, end):
+        if root < pre[i]:
+            mid = i
+            break
+
+    #left
+    post(start+1, mid)
+
+    #right
+    post(mid, end)
+
+    #root
+    print(root)
+
+post(0, len(pre))
