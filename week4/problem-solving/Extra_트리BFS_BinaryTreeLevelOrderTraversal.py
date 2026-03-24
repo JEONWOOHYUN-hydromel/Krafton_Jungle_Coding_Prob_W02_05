c00a16a10e8a14778cc2a4724e47386ef5c21a8f
woohyun <haeli0312@gmail.com>
2026-03-24 11:04:49 +0900

    feat: LeetCode 120번 완료

--- week4/problem-solving/Extra_트리BFS_BinaryTreeLevelOrderTraversal.py
+++ week4/problem-solving/Extra_트리BFS_BinaryTreeLevelOrderTraversal.py
@@ -1,2 +1,28 @@
 # 트리, BFS - Binary Tree Level Order Traversal
 # 문제 링크: https://leetcode.com/problems/binary-tree-level-order-traversal/
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        from collections import deque
+
+        result = []
+        if root is None:
+            return result
+        que = deque([[root]])
+        while que:
+            cur = que.popleft()
+            vals = []            
+            nxt = []
+            for c in cur:
+                vals.append(c.val)
+
+                if c.left is not None:
+                    nxt.append(c.left)
+
+                if c.right is not None:
+                    nxt.append(c.right)
+            
+            result.append(vals)
+            if nxt: 
+                que.append(nxt)
+        
+        return result
