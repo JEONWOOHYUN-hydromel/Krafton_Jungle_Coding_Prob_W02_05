13082bb890f51b75bcda9a4107bc205541de9bb6
woohyun <haeli0312@gmail.com>
2026-03-26 22:46:24 +0900

    feat: 백준 2748번 완료

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,22 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+import sys
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def fib(n, mem = None):
+    if n < 2:
+        return n
+    
+    if mem is None:
+        mem = {}
+    
+    if n in mem:
+        return mem[n]
+
+    newfib = fib(n-2, mem) + fib(n-1, mem)
+    mem[n] = newfib
+    return newfib
+
+print(fib(int(input())))
