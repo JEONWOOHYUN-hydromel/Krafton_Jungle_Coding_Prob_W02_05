3d0a30c12bd7b1a65bb7330962a0f09b0fa33497
woohyun <haeli0312@gmail.com>
2026-03-27 15:56:35 +0900

    feat: 백준 1904번 완료

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,15 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+if n < 3:
+    print(n)
+
+else:
+    a, b = 1, 2
+    for i in range(3, n+1):
+        a, b = b, (a+b)%15746
+    print(b)
