2851642a78bb83571ad457cd1c2dc2eb9893fbec
woohyun <haeli0312@gmail.com>
2026-03-28 01:08:52 +0900

    feat: 백준 9251번 완료

--- week5/problem-solving/난이도중_DP_LCS_골드5.py
+++ week5/problem-solving/난이도중_DP_LCS_골드5.py
@@ -1,2 +1,19 @@
 # DP - LCS (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9251
+import sys
+input = sys.stdin.readline
+
+str1 = input().strip()
+str2 = input().strip()
+l1 = len(str1)
+l2 = len(str2)
+
+arr = [[0]*(l1+1) for _ in range(l2+1)]
+
+for i in range(1, l2+1):
+    for j in range(1, l1+1):
+        if str1[j-1] == str2[i-1]:
+            arr[i][j] = arr[i-1][j-1] + 1
+        else:
+            arr[i][j] = max(arr[i-1][j], arr[i][j-1])
+print(arr[l2][l1])
