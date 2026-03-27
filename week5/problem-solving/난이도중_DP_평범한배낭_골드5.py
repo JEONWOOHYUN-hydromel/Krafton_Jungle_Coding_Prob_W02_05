2278f2f05eda4ec4b778812b4d3dea4fe0f4ba3d
woohyun <haeli0312@gmail.com>
2026-03-27 20:24:16 +0900

    feat: 백준 12865번 완료

--- week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
+++ week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
@@ -1,2 +1,14 @@
 # DP - 평범한 배낭 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/12865
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+lst_item = list(tuple(map(int, input().split())) for _ in range(n))
+dp = [0] * (k+1)
+
+for w, v in lst_item:
+    for i in range(k, w-1, -1):
+        dp[i] = max(dp[i], dp[i-w] + v)
+
+print(dp[k])
