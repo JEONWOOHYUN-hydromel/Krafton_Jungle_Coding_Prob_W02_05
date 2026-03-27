2612c7e3ed81f535194bd7729cb0d30c77ffb484
woohyun <haeli0312@gmail.com>
2026-03-27 17:03:05 +0900

    feat: 백준 1541번 완료

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,32 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+s = input()
+
+nums = []
+ops = []
+num = ""
+
+for ch in s:
+    if ch.isdigit():
+        num+=ch
+    else:
+        nums.append(int(num))
+        num = ""
+        ops.append(ch)
+
+nums.append(int(num))
+
+minusOn = False
+total = nums[0]
+for i, op in enumerate(ops):
+    if minusOn:
+        total -= nums[i+1]
+        continue
+
+    if op == "+":
+        total += nums[i+1]
+    elif op == "-":
+        minusOn = True
+        total -= nums[i+1]
+
+print(total)
