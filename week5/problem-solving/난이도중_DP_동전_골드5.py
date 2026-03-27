a503a4678700b6312a9b0ecb215f5f80db74d030
woohyun <haeli0312@gmail.com>
2026-03-28 00:02:43 +0900

    feat: 백준 9084번 완료

--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,19 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+import sys
+input = sys.stdin.readline
+
+num_case = int(input())
+for _ in range(num_case):
+    num_coin = int(input())
+    coins = list(map(int, input().split()))
+    target = int(input())
+
+    dp = [0]*(target+1)
+    dp[0] = 1
+
+    for coin in coins:
+        for i in range(coin, target+1):
+            dp[i] += dp[i-coin]
+
+    print(dp[target])
