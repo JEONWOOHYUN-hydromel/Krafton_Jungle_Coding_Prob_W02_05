6267695c6a06abafa36081908fc7d2c952bb8a37
woohyun <haeli0312@gmail.com>
2026-03-27 17:17:05 +0900

    feat: 백준 1931번 완료

--- week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
+++ week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
@@ -1,2 +1,17 @@
 # 그리디 - 회의실 배정 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1931
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+meets = [list(map(int, input().split())) for _ in range(n)]
+meets.sort(key= lambda x: (x[1], x[0]))
+
+cnt = 0
+prev_end = 0
+for meet in meets:
+    if meet[0] >= prev_end:
+        cnt += 1
+        prev_end = meet[1]
+
+print(cnt)
