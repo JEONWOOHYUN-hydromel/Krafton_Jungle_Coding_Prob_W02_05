cf301d111bb4ba8beb32ae4bb8419d67eb4245a6
woohyun <haeli0312@gmail.com>
2026-03-27 16:39:10 +0900

    feat: 백준 11047번 완료

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,15 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+
+cnt = 0
+for c in coins[::-1]:
+    if k//c > 0:
+        cnt += k//c
+        k %= c
+
+print(cnt)
